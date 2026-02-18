02f8e0a5977a2975eb1785a694292085b00ff19f
aliya-al <aliya11yakupova@gmail.com>
2026-02-18 15:48:51 +0300

    Adjust Excel import to create products without images

--- app/routers/admin/catalog_crud/__init__.py
+++ app/routers/admin/catalog_crud/__init__.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 
 from app.utils.deps import require_admin_or_404
-from . import brands, categories, products, attributes, requests
+from . import brands, categories, products, attributes, requests, products_import
 
 router = APIRouter(dependencies=[Depends(require_admin_or_404)])
 
@@ -10,3 +10,4 @@ router.include_router(brands.router)
 router.include_router(products.router)
 router.include_router(attributes.router)
 router.include_router(requests.router)
+router.include_router(products_import.router)
